9b04374b63e7a8fa64a05903094e2e2faf751c0c
Marcelo Lima <marcelo_sp5@hotmail.com>
2019-02-25 22:27:08 -0300

    BD configuration

---
+++ Data/Connection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace System.Windows
+{
+	class ConnectionBD
+	{
+		public void connect()
+		{
+			try
+			{
+				MySqlConnection connection = new MySqlConnection("server=localhost;port=3306;UserId=root;database=tradedb");
+				connection.Open();
+			}
+			catch (Exception erro)
+			{
+				MessageBox.Show(erro.Message);
+			}
+		}
+		
+	}
+}
